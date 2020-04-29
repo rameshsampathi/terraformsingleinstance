246b0d61082fa4182bd0135d747a682bfba34101
root <root@ip-10-50-2-172.ec2.internal>
2020-04-29 14:45:49 +0000

    first commit file

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 #This Terraform Code Deploys Basic VPC Infra.
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
+    #access_key = "${var.aws_access_key}"
+    #secret_key = "${var.aws_secret_key}"
     region = "${var.aws_region}"
+    profile = "default" 
 }
 
 resource "aws_vpc" "default" {
@@ -89,40 +90,3 @@ resource "aws_security_group" "allow_all" {
     cidr_blocks     = ["0.0.0.0/0"]
     }
 }
-
-data "aws_ami" "my_ami" {
-     most_recent      = true
-     #name_regex       = "^mavrick"
-     owners           = ["721834156908"]
-}
-
-
-# resource "aws_instance" "web-1" {
-#     ami = "${data.aws_ami.my_ami.id}"
-#     #ami = "ami-0d857ff0f5fc4e03b"
-#     availability_zone = "us-east-1a"
-#     instance_type = "t2.micro"
-#     key_name = "LaptopKey"
-#     subnet_id = "${aws_subnet.subnet1-public.id}"
-#     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-#     associate_public_ip_address = true	
-#     tags = {
-#         Name = "Server-1"
-#         Env = "Prod"
-#         Owner = "Sree"
-#     }
-# }
-
-#output "ami_id" {
-#  value = "${data.aws_ami.my_ami.id}"
-#}
-#!/bin/bash
-# echo "Listing the files in the repo."
-# ls -al
-# echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
-# packer build -var 'aws_access_key=XXXXXXXXXXXXX' -var 'aws_secret_key=YYYYYYYYYYYYYYYYYYYY' packer.json
-# echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
-#Jenkins-Terrafrom
-# terraform apply --var-file test.tfvars -var="aws_access_key=XXXXXXXXXXXXX" -var='aws_secret_key=YYYYYYYYYYYYYYYYYYYY' --auto-approve
-#AzureDevOps-Terraform
-#terraform plan --var-file terraform.tfvars -var="aws_access_key=xxxxxxxxx" -var="aws_secret_key=asdaddadasadad"
